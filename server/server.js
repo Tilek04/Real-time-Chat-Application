a7556116641d25a6ae776b9abb32985d7175c6b3
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2024-06-03 17:01:41 +0200

    start with dotenv

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.listen(5000, () => console.log("Server is running on 5000 PORT"));
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
