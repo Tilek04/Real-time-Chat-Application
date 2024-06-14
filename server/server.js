8e6e27ea9cf572df373a357331c37ad05a35e830
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2024-06-14 08:51:07 +0200

    Create models

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routers/auth.js";
-import connectToMongoDB from "../db/connectToMongoDB.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 dotenv.config();
