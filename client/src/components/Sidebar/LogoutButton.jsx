7e34f4736b0829f7dc70a810631b9488e1a4d47e
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2024-07-01 11:48:39 +0200

    Finish logout hook and functionality

--- client/src/components/Sidebar/LogoutButton.jsx
+++ client/src/components/Sidebar/LogoutButton.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../Hooks/useLogout";
 
 const LogoutButton = () => {
+  const { logout, loading } = useLogout();
   return (
     <div className="mt-auto">
-      <BiLogOut className="mt-4 w-6 h-6 text-white cursor-pointer" />
+      {!loading ? (
+        <BiLogOut
+          className="mt-4 w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <span className="loading loading-spinner"> </span>
+      )}
     </div>
   );
 };
--- client/src/pages/signup/SignUp.jsx
+++ client/src/pages/signup/SignUp.jsx
@@ -98,8 +98,14 @@ const SignUp = () => {
             Already have an account?
           </Link>
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Sign Up
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+              disabled={loading}>
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>
